0573c99af3601d0b0d0884fe9d78656838ca0546
Muhammed janees <janeesgraphicdesigner@gmail.com>
2023-01-30 12:44:10 +0530

    fav_model complteted

--- lib/Favorite/favorite_model.dart
+++ lib/Favorite/favorite_model.dart
@@ -1,12 +1,14 @@
-import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 part 'favorite_model.g.dart';
 
 @HiveType(typeId: 1)
 class FavModel {
   @HiveField(0)
   String id;
+
   @HiveField(1)
   String titile;
+
   @HiveField(2)
   String content;
   FavModel({required this.id, required this.titile, required this.content});
